47b3bb13e6443dfbabe1a1550abbe45b63ab18a7
fatimabk1 <fatima.kahbi@gmail.com>
2024-01-01 20:19:36 -0500

    Initial Commit

---
+++ MustacheFace/MustacheFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MustacheFaceApp.swift
+//  MustacheFace
+//
+//  Created by Fatima Kahbi on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MustacheFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
